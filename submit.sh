566384a7447f7eb3303cf283be8d155d1dfab6ce
margaritageleta <noticiasmundiales99@gmail.com>
2021-02-05 22:30:20 +0000

    slurm job script

---
+++ submit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sbatch <<EOT
+#!/bin/sh
+#SBATCH --job-name=VAEgen-$1
+#SBATCH -p gpi.compute
+#SBATCH --gres=gpu:1,gpumem:10G
+#SBATCH --mem=16G
+#SBATCH --time=12:00:00
+#SBATCH -o logs/exp_$1.log
+#SBATCH -e logs/exp_$1.err
+
+echo "Executing experiment $1"
+python3 src/trainer.py
+echo "Success!"
+EOT
+
+
+
+
+
