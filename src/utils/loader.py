969c2b5bd3f6233746189f081ca504098ced6bf7
margaritageleta <noticiasmundiales99@gmail.com>
2021-02-06 15:20:36 +0000

    data loader script

--- src/utils/loader.py
+++ src/utils/loader.py
@@ -1,9 +1,40 @@
 import os
+import time
 import torch
 import numpy as np
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from .decorators import timer 
 
-USER_PATH = os.environ.get('USER_PATH')
-DATA_PATH = os.path.join(USER_PATH, 'data')
+class GenDataset(Dataset):
+    def __init__(self, data, max_limit = 25000):
+        data = np.load(data, allow_pickle = True)
+        self.snps = data["snps"][:,:max_limit].astype(float)
+        self.pops = data["populations"][:max_limit]
+        # self.spops = data["subpopulations"][:max_limit]
+        self._max_limit = max_limit
 
-print(DATA_PATH)
+        print(self.snps.shape)
+        print(self.pops.shape)
+
+        pops = ['AFR', 'EAS', 'EUR', 'AMR', 'OCE', 'SAS', 'WAS']
+        self.mapper = dict(zip(pops, np.arange(0, len(pops))))
+
+    def __len__(self):
+        return self.snps.shape[0]
+
+    def __getitem__(self, index):
+        snps_array = self.snps[index,:]
+        snps_array[np.where(snps_array == 0)] = -1
+        snps_array = torch.tensor(snps_array).float()
+        pop_id = torch.tensor(self.mapper[self.pops[index]]).int()
+        # spop_id = torch.tensor(self.spops[index]).int()
+        return (snps_array, pop_id)
+
+@timer
+def loader(DATA_PATH = os.path.join(os.environ.get('USER_PATH'), 'data/ancestry_datasets'), batch_size = 10):
+    dataset = GenDataset(data = f'{DATA_PATH}/All_chm_World/all_chm_combined_snps_world_2M_with_labels.npz')
+    dataloader = DataLoader(dataset, batch_size = batch_size, shuffle = True, pin_memory = True)
+    return dataloader
+    
+
