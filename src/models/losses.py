4abc2913d6551285c0cec9747f6a9b0d113347fd
margaritageleta <noticiasmundiales99@gmail.com>
2021-02-07 13:52:11 +0000

    implement zeros and ones losses

--- src/models/losses.py
+++ src/models/losses.py
@@ -5,20 +5,30 @@ import torch.nn.functional as F
 
 def VAEloss(x, o, mu, logvar, beta=1):
 
-    loss = F.binary_cross_entropy(o, x, reduction='sum')
+    loss = F.binary_cross_entropy(o, x, reduction='mean')
     KL_divergence = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
  
     return loss +  beta * KL_divergence, loss, KL_divergence
 
-def L1loss(x, o, partial=False):
+def L1loss(x, o, partial=False, proportion=False):
     loss = F.l1_loss((o > 0.5).float(), x, reduction='sum')
-
+    
     if partial:
 
         one_hot_neg = (x - (o.detach() > 0.5).float()).flatten()
         loss_zeros = len(np.where(one_hot_neg == -1)[0])
         loss_ones = len(np.where(one_hot_neg == 1)[0])
+
+        if proportion: 
+            zeros = len(np.where(x == 0)[0])
+            ones = len(np.where(x == 1)[0])
+            
+            acc_total = loss / (x.shape[0] * x.shape[1]) * 100
+            acc_zeros = loss_zeros / zeros * 100
+            acc_ones = loss_ones / ones * 100
+
+            return acc_total, acc_zeros, acc_ones
         
         return loss, loss_zeros, loss_ones
     
-    return loss
+    return loss if not proportion else loss / (x.shape[0] * x.shape[1]) * 100
