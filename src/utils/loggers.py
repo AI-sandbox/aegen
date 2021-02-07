cf337c716e024d499f987a29ff8c62bb26d966ed
margaritageleta <noticiasmundiales99@gmail.com>
2021-02-07 11:59:25 +0000

    add loggers

---
+++ src/utils/loggers.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def progress(current, total, batch_size, bar=True, time=None, **kwargs):
+
+    indicator = f'Progress: [{current} / {total}]'
+    bar_progress = ''
+    metrics = ''
+
+    if bar:
+        bar_progress += '=' * (current) + '-' * (total - current) 
+    
+    if time is not None:
+        bar_progress += f'({np.round(time, 2)}s)'
+
+    for k, v in kwargs.items():
+        metrics += f' {k}: {np.round(np.mean(v) / batch_size, 2)}'
+    
+    print(f'{indicator} {bar_progress} {metrics}')
+
+    
