962d1c9179640ebd64b36244a6f4252886a51ee4
margaritageleta <noticiasmundiales99@gmail.com>
2021-02-07 13:51:28 +0000

    get rid of manual batch size div

--- src/utils/loggers.py
+++ src/utils/loggers.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def progress(current, total, batch_size, bar=True, time=None, **kwargs):
+def progress(current, total, batch_size=None, bar=True, time=None, **kwargs):
 
     indicator = f'Progress: [{current} / {total}]'
     bar_progress = ''
@@ -14,7 +14,7 @@ def progress(current, total, batch_size, bar=True, time=None, **kwargs):
         bar_progress += f'({np.round(time, 2)}s)'
 
     for k, v in kwargs.items():
-        metrics += f' {k}: {np.round(np.mean(v) / batch_size, 2)}'
+        metrics += f' {k}: {np.round(np.mean(v), 2)}'
     
     print(f'{indicator} {bar_progress} {metrics}')
 
