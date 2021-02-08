081b9299df5c9d185d5b52871cff868c396efd73
margaritageleta <noticiasmundiales99@gmail.com>
2021-02-08 13:42:23 +0000

    holdout created

--- src/utils/assemblers.py
+++ src/utils/assemblers.py
@@ -4,6 +4,13 @@ import numpy as np
 from decorators import timer
 
 def encode(catvar):
+    """
+    Encodes a categorical variable with strings into a numeric vector.
+    Needed for hdf5 storage.
+
+    Inputs:
+        catvar: the categorical variable to encode.
+    """
     cat = np.unique(catvar)
     ordinal = dict(zip(cat, np.arange(0, len(cat))))
     mapper = np.vectorize(lambda x: ordinal[x])
@@ -13,6 +20,12 @@ def encode(catvar):
 def npy2hdf5(ipath, opath, max_limit=25000, max_variance=True):
     """
     Converts a SNPs .npy file into a .hdf5 dataset.
+
+    Inputs:
+        ipath: location of SNPs data in .npz file.
+        opath: location to store the .hdf5 file.
+        max_limit: maximum number of SNPs to use.
+        max_variance: select SNPs with maximum variance.
     """
     print('Loading npy...')
     data = np.load(ipath, mmap_mode='r+', allow_pickle=True)
@@ -40,8 +53,7 @@ def npy2hdf5(ipath, opath, max_limit=25000, max_variance=True):
     h5f.create_dataset('snps', data=snps)
     h5f.create_dataset('populations', data=populations)
     h5f.create_dataset('subpopulations', data=subpopulations)
-    print('Done.')
-    print(h5f.keys())
+    print('Done.\n')
 
     h5f.close()
 
@@ -51,6 +63,11 @@ def splitter(arr, ratios, seed=123):
     Note: ratios must be a 'tuple'.
 
     Returns a list with the array splits by ratio.
+
+    Inputs:
+        arr: the array to split.
+        ratios: a tuple with the % to split.
+        seed: random seed to permute the array before splitting.
     """
 
     np.random.seed(int(seed))
@@ -65,6 +82,12 @@ def holdout_by_pop(snps, populations, *ratios, seed=123):
     The ratios for partitions are passed by 'ratios' parameter.
 
     Returns the sets with proportional subsets of each population.
+
+    Inputs:
+        snps: the SNPs matrix.
+        populations: indices of the corresponding population of each SNP array.
+        ratios: % to split.
+        seed: random seed to permute the array before splitting and shuffling.
     """
     
     _r = len(ratios)
@@ -105,11 +128,21 @@ if __name__ == '__main__':
         max_variance=True,
     )
 
+    h5f = h5py.File(os.path.join(os.environ.get('USER_PATH'), 'data/prepared/single_ancestry/data.h5'), 'r')
+    snps = h5f['snps'][:]
+    populations = h5f['populations'][:]
+    h5f.close()
 
+    train, valid = holdout_by_pop(snps, populations, 0.8, 0.2, seed=123)
 
+    print('Storing train.hdf5...')
+    h5f = h5py.File(os.path.join(os.environ.get('USER_PATH'), 'data/prepared/single_ancestry/train.h5'), 'w')
+    h5f.create_dataset('train', data=train)
+    h5f.close()
+    print('Done.\n')
 
-
-
-
-    
-
+    print('Storing valid.hdf5...')
+    h5f = h5py.File(os.path.join(os.environ.get('USER_PATH'), 'data/prepared/single_ancestry/valid.h5'), 'w')
+    h5f.create_dataset('valid', data=valid)
+    h5f.close()
+    print('Done.\n')
