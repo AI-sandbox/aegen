cff7f8d33371d655974325bf5c589d206792fb1e
margaritageleta <noticiasmundiales99@gmail.com>
2021-02-08 13:27:32 +0000

    preprocessing utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,4 @@
 from .decorators import timer
 from .loader import SNPs, loader
 from .loggers import progress
+from .assemblers import npy2hdf5
--- src/utils/assemblers.py
+++ src/utils/assemblers.py
@@ -11,6 +11,9 @@ def encode(catvar):
 
 @timer
 def npy2hdf5(ipath, opath, max_limit=25000, max_variance=True):
+    """
+    Converts a SNPs .npy file into a .hdf5 dataset.
+    """
     print('Loading npy...')
     data = np.load(ipath, mmap_mode='r+', allow_pickle=True)
     print('Loaded.')
@@ -41,6 +44,58 @@ def npy2hdf5(ipath, opath, max_limit=25000, max_variance=True):
     print(h5f.keys())
 
     h5f.close()
+
+def splitter(arr, ratios, seed=123):
+    """
+    Splits an array by ratios.
+    Note: ratios must be a 'tuple'.
+
+    Returns a list with the array splits by ratio.
+    """
+
+    np.random.seed(int(seed))
+    arr = np.random.permutation(arr)
+    ind = np.add.accumulate(np.array(ratios) * len(arr)).astype(int)
+    return [x.tolist() for x in np.split(arr, ind)][:len(ratios)]
+
+@timer
+def holdout_by_pop(snps, populations, *ratios, seed=123):
+    """
+    Creates a holdout partition by equal proportions for each population.
+    The ratios for partitions are passed by 'ratios' parameter.
+
+    Returns the sets with proportional subsets of each population.
+    """
+    
+    _r = len(ratios)
+    _sets = [None] * _r
+    
+    for id, pop in enumerate(np.unique(populations)):
+        pop_idx = np.where(np.asarray(populations) == pop)[0]
+        _subsets = splitter(pop_idx, ratios, seed=seed)
+        
+        print(f'Population #{pop} has {len(pop_idx)} samples.\n'+'-' * 50)
+        for i in range(_r):
+            print(f'Subsetting {ratios[i] * 100}%, {len(_subsets[i])} samples.')
+            if id == 0:
+                _sets[i] = snps[_subsets[i],:]
+                print(' ' * 5 + f'Stored {_sets[i].shape} matrix in set #{i}.')
+            else:
+                _subset_i = snps[_subsets[i],:]
+                _sets[i] = np.vstack((_sets[i],_subset_i))
+                print(' ' * 5 + f'Stored {_subset_i.shape} matrix in set #{i}.')
+                print(' ' * 5 + f'In total {_sets[i].shape} matrix in set #{i}.')
+        print('\n')
+    
+    print('Shuffling each set...\n')
+    for i in range(_r):
+        np.random.shuffle(_sets[i])
+        
+    print('-' * 50 + f'\nTotal counts:\n'+'-' * 50)
+    for i in range(_r):
+        print(f'{_sets[i].shape[0]} samples in set #{i}.')
+    
+    return _sets
 
 if __name__ == '__main__':
     npy2hdf5(
