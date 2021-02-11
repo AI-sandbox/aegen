5bc833f4f71978c50d8c9f4e6ad69a8c2c8c21b4
margaritageleta <noticiasmundiales99@gmail.com>
2021-02-11 18:55:19 +0000

    Instant bash script

---
+++ instant.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Executing instant experiment $1"
+mkdir -p $OUT_PATH/exp_$1
+cp params.json $OUT_PATH/exp_$1/
+srun -u --gres=gpu:1,gpumem:16G -p gpi.compute -c 4 --time 23:59:59 --mem 80GB python3 src/trainer.py --params $OUT_PATH/exp_$1/params.json
+echo "Success!"
