6c75fa4073df5a8dffd88d56d748d30792c791d7
margaritageleta <noticiasmundiales99@gmail.com>
2021-03-03 21:46:25 +0000

    Add pops in loader

--- src/utils/loader.py
+++ src/utils/loader.py
@@ -9,38 +9,40 @@ from torch.utils.data import DataLoader
 from utils.decorators import timer 
 
 class SNPs(Dataset):
-    def __init__(self, ipath, split_set='train'):
+    def __init__(self, ipath, split_set='train', ksize=5):
         """
         Inputs:
             split_set: to load.
+            ksize: size of SNP arrays in terms of thousands.
         """
 
-        h5f = h5py.File(os.path.join(ipath, f'{split_set}5K.h5'), 'r')
+        h5f = h5py.File(os.path.join(ipath, f'{split_set}{ksize}K.h5'), 'r')
         self.snps = h5f['snps'][:].astype(float)
+        self.populations = h5f['populations'][:].astype(int)
         h5f.close()
-
-        print(self.snps.shape)
 
     def __len__(self):
         return self.snps.shape[0]
 
     def __getitem__(self, index):
         snps_array = self.snps[index,:]
+        label = self.populations[index]
         # snps_array[np.where(snps_array == 0)] = -1 for denoising/imputation VAE
         snps_array = torch.from_numpy(snps_array).float()
         # return (snps_array, pop_id)
-        return snps_array
+        return (snps_array, label)
 
 @timer
-def loader(ipath, batch_size, split_set='train'):
-    dataset = SNPs(ipath=ipath, split_set=split_set)
+def loader(ipath, batch_size, split_set='train', ksize=5):
+    dataset = SNPs(ipath=ipath, split_set=split_set, ksize=ksize)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
     return dataloader
 
 if __name__ == '__main__':
     data = loader(
-        ipath=os.path.join(os.environ.get('USER_PATH'), 'data/chr22/prepared'),
+        ipath=os.path.join(os.environ.get('IN_PATH'), 'data/chr22/prepared'),
         batch_size=64, 
-        split_set='train'
+        split_set='train',
+        ksize=5
     )
-    print(data)
+    
