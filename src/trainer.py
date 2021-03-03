214738d4667374fa40fa06ab1d2b4d6d6803e5d6
margaritageleta <noticiasmundiales99@gmail.com>
2021-03-03 23:16:03 +0000

    Tester script

--- src/trainer.py
+++ src/trainer.py
@@ -1,24 +1,26 @@
 import os
 import time
 import json
+import yaml
 import wandb
 import torch
 import datetime
 import argparse
 import numpy as np
+import matplotlib.pyplot as plt
 from models.VAEgen import VAEgen
 from models.losses import VAEloss, L1loss
 from models.initializers import init_xavier
 from utils.loader import loader 
 from utils.decorators import timer 
-from utils.loggers import progress
+from utils.loggers import progress, latentPCA
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--params', 
                         type=str, 
-                        default=os.path.join(os.environ.get('OUT_PATH'), 'params.json'), 
-                        metavar='JSON_FILE',
-                        help='JSON file with parameters and hyperparameters'
+                        default=os.path.join(os.environ.get('USER_PATH'), 'params.yaml'), 
+                        metavar='YAML_FILE',
+                        help='YAML file with parameters and hyperparameters'
                     )
 parser.add_argument('--experiment', 
                         type=str, 
@@ -26,11 +28,24 @@ parser.add_argument('--experiment',
                         metavar='SUMMARY',
                         help='Summary of the experiment'
                     )
+parser.add_argument('--num', 
+                        type=int, 
+                        default=0, 
+                        metavar='NUMBER',
+                        help='NUMBER of the experiment'
+                    )                 
 parser.add_argument('--verbose', 
                         type=bool, 
                         default=False, 
                         metavar='BOOL',
                         help='Verbose output indicator'
+                    )
+
+parser.add_argument('--evolution', 
+                        type=bool, 
+                        default=False, 
+                        metavar='BOOL',
+                        help='Evolution plots at wandb'
                     )
 
 def save_checkpoint(state, is_best, filename=os.path.join(os.environ.get('USER_PATH'), 'checkpoints/checkpoint.pt')):
@@ -42,7 +57,7 @@ def save_checkpoint(state, is_best, filename=os.path.join(os.environ.get('USER_P
          print ("=> Loss did not improve")
 
 @timer
-def train(model, tr_loader, vd_loader, hyperparams, summary=None, verbose=False):
+def train(model, tr_loader, vd_loader, hyperparams, summary=None, num=0, verbose=False, slide=50, ts_loader=None):
 
     model.apply(init_xavier)
     wandb.init(project='VAEgen')
@@ -79,9 +94,9 @@ def train(model, tr_loader, vd_loader, hyperparams, summary=None, verbose=False)
         epoch_vae_loss, epoch_rec_loss, epoch_KL_div  = [], [], []
         epoch_L1_loss, epoch_zeros_loss, epoch_ones_loss = [], [], []
         
-        for i, snps_array in enumerate(tr_loader):
+        for i, batch in enumerate(tr_loader):
 
-            snps_array = snps_array.to(device)
+            snps_array = batch[0].to(device)
 
             snps_reconstruction, latent_mu, latent_logvar = model(snps_array)
             loss, rec_loss, KL_div = VAEloss(snps_array, snps_reconstruction, latent_mu, latent_logvar)
@@ -109,13 +124,13 @@ def train(model, tr_loader, vd_loader, hyperparams, summary=None, verbose=False)
                     L1_loss=epoch_L1_loss, zeros_loss=epoch_zeros_loss, ones_loss=epoch_ones_loss
                 )
 
-        total_vae_loss.append(np.mean(epoch_vae_loss))
-        total_rec_loss.append(np.mean(epoch_rec_loss))
-        total_KL_div.append(np.mean(epoch_KL_div))
+        total_vae_loss.append(np.mean(epoch_vae_loss[-slide:]))
+        total_rec_loss.append(np.mean(epoch_rec_loss[-slide:]))
+        total_KL_div.append(np.mean(epoch_KL_div[-slide:]))
 
-        total_L1_loss.append(np.mean(epoch_L1_loss))
-        total_zeros_loss.append(np.mean(epoch_zeros_loss))
-        total_ones_loss.append(np.mean(epoch_ones_loss))
+        total_L1_loss.append(np.mean(epoch_L1_loss[-slide:]))
+        total_zeros_loss.append(np.mean(epoch_zeros_loss[-slide:]))
+        total_ones_loss.append(np.mean(epoch_ones_loss[-slide:]))
 
         wandb.log({
             'tr_VAE_loss': total_vae_loss[-1],
@@ -132,6 +147,10 @@ def train(model, tr_loader, vd_loader, hyperparams, summary=None, verbose=False)
         print(f"Epoch [{epoch + 1} / {hyperparams['epochs']}] ({time.time()-ini}s) VAE error: {total_vae_loss[-1]}")
 
         validate(model, vd_loader, epoch, verbose)
+
+        if (ts_loader is not None) and (epoch % 10 == 0): #and (epoch != 0):
+            print('Testing...')
+            test(model, ts_loader, epoch)
         
         is_best = bool(total_vae_loss[-1] < best_loss)
         if is_best:
@@ -147,7 +166,7 @@ def train(model, tr_loader, vd_loader, hyperparams, summary=None, verbose=False)
                 'l1_losses': total_L1_loss,
                 'zeros_losses': total_zeros_loss,
                 'ones_losses': total_ones_loss,
-            }, is_best = is_best, filename=os.path.join(os.environ.get('USER_PATH'), f'checkpoints/checkpoint_VAE.pt'))
+            }, is_best = is_best, filename=os.path.join(os.environ.get('USER_PATH'), f'checkpoints/checkpoint_VAE_{num}.pt'))
     
     print(f'Training finished in {time.time() - ini}s.')
 
@@ -169,9 +188,9 @@ def validate(model, vd_loader, epoch, verbose=False):
     ini = time.time()
     with torch.no_grad():
         print('Validating current model...')
-        for i, snps_array in enumerate(vd_loader):
+        for i, batch in enumerate(vd_loader):
 
-            snps_array = snps_array.to(device)
+            snps_array = batch[0].to(device)
 
             snps_reconstruction, latent_mu, latent_logvar = model(snps_array)
 
@@ -205,25 +224,57 @@ def validate(model, vd_loader, epoch, verbose=False):
                 L1_loss=total_L1_loss, zeros_loss=total_zeros_loss, ones_loss=total_ones_loss
             )
 
+@timer
+def test(model, ts_loader, epoch):
+
+    device  = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f'Using device: {device}')
+
+    model.to(device)
+
+    # Set to evaluation mode
+    model.eval()
+
+    original, latent, labels = np.empty((0, model.encoder.FCs[0].FC[0].in_features), int), np.empty((0, model.decoder.FCs[0].FC[0].in_features), int), torch.Tensor([])
+    for i, batch in enumerate(ts_loader):
+        original = np.vstack((original, batch[0]))
+        mu, _ = vae.encoder(batch[0].to(device))
+        latent = np.vstack((latent, mu.detach().cpu().squeeze(0)))
+        labels = torch.cat((labels, batch[1]))
+
+    fig = latentPCA(original, latent, labels.int())
+    wandb.log({f"Latent space at epoch {epoch}": fig})
+    plt.close('all')
+
 if __name__ == '__main__':
 
     args = parser.parse_args()
     with open(args.params, 'r') as f:
-        params = json.load(f)
+        params = yaml.safe_load(f)
     model_params = params['model']
     hyperparams = params['hyperparams']
+    ksize=int(model_params['encoder']['input']['size'] / 1000)
     
     tr_loader = loader(
         ipath=os.path.join(os.environ.get('IN_PATH'), 'data/chr22/prepared'),
         batch_size=hyperparams['batch_size'], 
-        split_set='train'
+        split_set='train',
+        ksize=ksize
     )
 
     vd_loader = loader(
         ipath=os.path.join(os.environ.get('IN_PATH'), 'data/chr22/prepared'),
         batch_size=hyperparams['batch_size'], 
-        split_set='valid'
+        split_set='valid',
+        ksize=ksize
     )
+    
+    ts_loader = loader(
+        ipath=os.path.join(os.environ.get('IN_PATH'), 'data/chr22/prepared'),
+        batch_size=hyperparams['batch_size'], 
+        split_set='test',
+        ksize=ksize
+    ) if bool(args.evolution) else None
     
     vae = VAEgen(params=model_params)
 
@@ -233,6 +284,8 @@ if __name__ == '__main__':
         vd_loader=vd_loader,
         hyperparams=hyperparams,
         summary=args.experiment,
+        num=args.num,
         verbose=bool(args.verbose),
+        slide=15,
+        ts_loader=ts_loader
     )
-    
