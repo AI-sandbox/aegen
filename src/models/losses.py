bb05d14ec80724973aeb25006ba0b50653721bf6
margaritageleta <noticiasmundiales99@gmail.com>
2021-03-04 13:35:04 +0000

    Add compression ratio metric

--- src/models/losses.py
+++ src/models/losses.py
@@ -18,17 +18,19 @@ def L1loss(x, o, partial=False, proportion=False):
         one_hot_neg = (x - (o.detach() > 0.5).float()).flatten()
         loss_zeros = len(np.where(one_hot_neg == -1)[0])
         loss_ones = len(np.where(one_hot_neg == 1)[0])
+        ones = len(np.where(x == 1)[0])
+        compression_ratio = loss / ones * 100
 
         if proportion: 
             zeros = len(np.where(x == 0)[0])
-            ones = len(np.where(x == 1)[0])
             
             acc_total = loss / (x.shape[0] * x.shape[1]) * 100
             acc_zeros = loss_zeros / zeros * 100
             acc_ones = loss_ones / ones * 100
+            
 
-            return acc_total, acc_zeros, acc_ones
+            return acc_total, acc_zeros, acc_ones, compression_ratio
         
-        return loss, loss_zeros, loss_ones
+        return loss, loss_zeros, loss_ones, compression_ratio
     
     return loss if not proportion else loss / (x.shape[0] * x.shape[1]) * 100
