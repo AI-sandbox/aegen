e41f0da870bb7a5f0a4d110b95887cdc36088b6e
margaritageleta <noticiasmundiales99@gmail.com>
2021-03-23 19:34:55 +0000

    Split founders for simulation script

--- simulate.sh
+++ simulate.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+dataset=("train" "valid" "test")
+population=("EUR" "EAS" "AMR" "SAS" "AFR" "OCE" "WAS")
+for set in "${dataset[@]}"
+do mkdir -p $OUT_PATH/data/chr22/prepared/$set
+    for pop in "${population[@]}"
+    do mkdir -p $OUT_PATH/data/chr22/prepared/$set/$pop; 
+       mkdir -p $OUT_PATH/data/chr22/prepared/$set/$pop/generations
+    done
+done
+python3 $USER_PATH/src/utils/mapper.py
 echo "Generating $1 generations with $2 individuals in each population"
-srun -u -p gpi.develop -c 1 --time 02:00:00 --mem 80GB python3 src/pyadmix/admix.py \
+if srun -u -p gpi.develop -c 4 --time 02:00:00 --mem 80GB \
+python3 $USER_PATH/src/pyadmix/admix.py \
     $IN_PATH/data/chr22/ref_final_beagle_phased_1kg_hgdp_sgdp_chr22_hg19.vcf.gz \
-    $IN_PATH/data/chr22/prepared/ $1 $2
+    $OUT_PATH/data/chr22/prepared/ $1 $2; then
+    echo "Success!"
+else echo 'Fail!'; fi
---
+++ src/utils/mapper.py
@@ -0,0 +1,31 @@
+import os
+import logging
+import numpy as np
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    ratios = [0.8, 0.1, 0.1]
+    for ancestry in ['EUR', 'EAS', 'AMR', 'SAS', 'AFR', 'OCE', 'WAS']:
+        log.info(f'Splitting founders of {ancestry}')
+        ancestry_df = pd.read_csv(
+            os.path.join(os.environ.get('IN_PATH'), f'data/chr22/{ancestry}.map'),
+            delimiter="\t",
+            header=None,
+            comment="#",
+            dtype=str
+        )
+        data = ancestry_df.sample(frac=1, random_state=123)
+        proportions = np.add.accumulate(np.array(ratios) * data.shape[0]).astype(int)
+        
+        train, valid, test = np.split(data, proportions[:-1])
+
+        log.info(f'\t{train.shape[0]} founders for training')
+        log.info(f'\t{valid.shape[0]} founders for validation')
+        log.info(f'\t{test.shape[0]} founders for test')
+
+        train.to_csv(os.path.join(os.environ.get('OUT_PATH'), f'data/chr22/prepared/train/{ancestry}/{ancestry}.map'), sep="\t", header=None, index=False)
+        valid.to_csv(os.path.join(os.environ.get('OUT_PATH'), f'data/chr22/prepared/valid/{ancestry}/{ancestry}.map'), sep="\t", header=None, index=False)
+        test.to_csv(os.path.join(os.environ.get('OUT_PATH'), f'data/chr22/prepared/test/{ancestry}/{ancestry}.map'), sep="\t", header=None, index=False)
