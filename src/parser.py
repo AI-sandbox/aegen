188daabb9207f0eeebd75a1b2bbe4618d1bb0a54
margaritageleta <noticiasmundiales99@gmail.com>
2021-03-26 18:44:24 +0000

    Parser script

---
+++ src/parser.py
@@ -0,0 +1,52 @@
+import os
+import argparse
+
+def parse_bool(boolean):
+    if isinstance(boolean, bool):
+       return keyword
+    if boolean.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif boolean.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Wrong keyword.')
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--params', 
+        type=str, 
+        default=os.path.join(os.environ.get('USER_PATH'), 'params.yaml'), 
+        metavar='STR',
+        help='Path of YAML file with parameters and hyperparameters defined in [STR]'
+    )
+    parser.add_argument('--experiment', 
+        type=str, 
+        default=None, 
+        metavar='STR',
+        help='Summary of the experiment defined in [STR]'
+    )
+    parser.add_argument('--num', 
+        type=int, 
+        default=0, 
+        metavar='INT',
+        help='#[INT] of the experiment'
+    )                 
+    parser.add_argument('--verbose', 
+        type=parse_bool, 
+        default=False, 
+        metavar='BOOL',
+        help='Verbose output indicator defined by [BOOL]'
+    )
+    parser.add_argument('--evolution', 
+        type=parse_bool, 
+        default=False, 
+        metavar='BOOL',
+        help='PCA evolution plots at wandb defined by [BOOL]'
+    )
+    parser.add_argument('--only', 
+        type=int, 
+        default=None, 
+        metavar='INT',
+        help='Use data only from population by label [INT]'
+    )
+    return parser
