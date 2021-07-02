facdf48ffabaa45016538ae31095028e314b1415
3omni <noticiasmundiales99@gmail.com>
2021-07-02 04:03:00 -0700

    Add multiple distributions to AE

--- src/models/VAEgen.py
+++ src/models/VAEgen.py
@@ -5,13 +5,15 @@ import torch.nn as nn
 class FullyConnected(nn.Module):
     def __init__(self, input, output, dropout=0, normalize=True, activation=None):
         super().__init__()  
-
         modules = []
+        ## Append dropout if desired.
         if dropout is not None and dropout > 0:
             modules.append(nn.AlphaDropout(p=dropout))
         modules.append(nn.Linear(input, output))
+        ## Normalize layer if desired.
         if normalize:
             modules.append(nn.BatchNorm1d(output))
+        ## Add activation function if desired.
         if activation is not None:
             if activation == 'ReLU':
                 modules.append(nn.ReLU())
@@ -29,41 +31,47 @@ class FullyConnected(nn.Module):
 
 
 class Encoder(nn.Module):
-    def __init__(self, params, num_classes=None):
+    def __init__(self, latent_distribution, params, num_classes=None):
         super().__init__()
+        self.latent_distribution = latent_distribution
         modules = []
-        depth = len(params.keys()) - 2
+        depth = len(params.keys()) if self.latent_distribution != 'Gaussian' else len(params.keys()) - 1
         for i in range(depth):
             modules.append(FullyConnected(
-                input=(
-                    params['input']['size'] if num_classes is None else (params['input']['size'] + num_classes)
+                ## Only can condition input (layer0).
+                input = (
+                    params[f'layer{i}']['size'] if num_classes is None else (params['layer']['size'] + num_classes)
                 ) if i == 0 else params[f'hidden{i}']['size'],
-                output=params[f'hidden{i + 1}']['size'],
-                dropout=params['input']['dropout'] if i == 0 else params[f'hidden{i}']['dropout'],
-                normalize=params['input']['normalize'] if i == 0 else params[f'hidden{i}']['normalize'],
-                activation=params['input']['activation'] if i == 0 else params[f'hidden{i}']['activation'],
+                output = params[f'layer{i + 1}']['size'],
+                dropout = params[f'layer{i}']['dropout'],
+                normalize = params[f'layer{i}']['normalize'],
+                activation = params[f'layer{i}']['activation'],
             ))
         self.FCs = nn.Sequential(*modules)
-        self.FCmu = FullyConnected(
-            input=params['input']['size'] if depth == 0 else params[f'hidden{depth}']['size'],
-            output=params[f'hidden{depth + 1}']['size'],
-            dropout=params['input']['dropout'] if depth == 0 else params[f'hidden{depth}']['dropout'],
-            normalize=params['input']['normalize'] if depth == 0 else params[f'hidden{depth}']['normalize'],
-            activation=params['input']['activation'] if depth == 0 else params[f'hidden{depth}']['activation'],
-        )
-        self.FClogvar = FullyConnected(
-            input=params['input']['size'] if depth == 0 else params[f'hidden{depth}']['size'],
-            output=params[f'hidden{depth + 1}']['size'],
-            dropout=params['input']['dropout'] if depth == 0 else params[f'hidden{depth}']['dropout'],
-            normalize=params['input']['normalize'] if depth == 0 else params[f'hidden{depth}']['normalize'],
-            activation=params['input']['activation'] if depth == 0 else params[f'hidden{depth}']['activation'],
-        )
+        if self.latent_distribution == 'Gaussian':
+            self.FCmu = FullyConnected(
+                input = params[f'layer{depth}']['size'],
+                output=params[f'layer{depth + 1}']['size'],
+                dropout=params[f'layer{depth}']['dropout'],
+                normalize=params[f'layer{depth}']['normalize'],
+                activation=params[f'layer{depth}']['activation'],
+            )
+            self.FClogvar = FullyConnected(
+                input=params[f'layer{depth}']['size'],
+                output=params[f'layer{depth + 1}']['size'],
+                dropout=params[f'layer{depth}']['dropout'],
+                normalize=params[f'layer{depth}']['normalize'],
+                activation=params[f'layer{depth}']['activation'],
+            )
 
     def forward(self, x, c=None):
         o = self.FCs(x if c is None else torch.cat([x, c], 1))
-        o_mu = self.FCmu(o)
-        o_var = self.FClogvar(o)
-        return o_mu, o_var
+        if self.distribution == 'Gaussian':
+            o_mu = self.FCmu(o)
+            o_var = self.FClogvar(o)
+            return o_mu, o_var
+        else:
+            return o
 
 class Decoder(nn.Module):
     def __init__(self, params, num_classes=None):
@@ -88,32 +96,61 @@ class Decoder(nn.Module):
         return o
 
 class VAEgen(nn.Module):
-    def __init__(self, params, conditional=False, imputation=False):
+    def __init__(self, params, conditional=False, sample_mode=False):
         super().__init__()
-        
-        self.imputation, self.missing = imputation, params['missing']
+        ## AE shape can be:
+        ## - global: regular MLP.
+        ## - window-based: independent MLPs by windows.
+        ## - hybrid: independent MLP combined into one.
+        self.shape = params['shape']
+        ## Latent space distrubution can be:
+        ## - Gaussian: regular VAE.
+        ## - Bernoulli: LBAE.
+        ## - Uniform: VQ-VAE.
+        self.latent_distribution = params['distribution']
+        ## Encoder is defined by:
+        ## - Latent distribution.
+        ## - Parameters: layers' definitions.
+        ## - Num_classes: if conditional Encoder.
         self.encoder = Encoder(
+            latent_distribution = self.latent_distribution,
             params=params['encoder'], 
-            num_classes=params['num_classes'] if conditional else None
-        ) 
+            #num_classes=params['num_classes'] if conditional else None
+        )
+        ## Decoder is defined by:
+        ## - Parameters: layers' definitions.
+        ## - Num_classes: if conditional Decoder.
         self.decoder = Decoder(
             params=params['decoder'], 
-            num_classes=params['num_classes'] if conditional else None
+            #num_classes=params['num_classes'] if conditional else None
         ) 
     
+    ## Variational Auto-encoder: Gaussian latent space
     def reparametrize(self, mu, logvar):
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-        return mu + std * eps
+        if self.training or sample_mode:
+            std = torch.exp(0.5 * logvar)
+            eps = torch.randn_like(std)
+            return mu + std * eps
+        else: return mu
+        
+    # def sample(self, )
 
     def forward(self, x, c=None):
-        if self.imputation: 
-            x, sz = 2 * x - 1, x.shape
-            if self.missing > 0:
-                mask = torch.from_numpy((np.random.uniform(size=np.prod(sz)) < self.missing)).type(torch.bool).reshape(sz).cuda()
-                x[mask] = 0
-        z_mu, z_logvar = self.encoder(x, c)
-        z = self.reparametrize(z_mu, z_logvar)
-        o = self.decoder(z, c)
-        if self.imputation and self.missing > 0: return o, z_mu, z_logvar, mask
-        else: return o, z_mu, z_logvar
+        ## VAE encoder outputs 2 feature maps:
+        ## - Mu feature.
+        ## - Logvar feature.
+        ## These 2 feature maps are reparametrized
+        ## to yield z - the latent factors' vector.
+        if self.distribution == 'Gaussian':
+            z_mu, z_logvar = self.encoder(x, c)
+            z = self.reparametrize(z_mu, z_logvar)
+            o = self.decoder(z, c)
+            return o, z_mu, z_logvar
+        ## LBAE 
+        elif self.distribution == 'Bernoulli':
+            raise('Not implemented.')
+        ## VQ-VAE
+        elif self.distribution == 'Uniform':
+            raise('Not implemented.')
+        ## Unknown distribution
+        else: raise('Unknown distribution.')
