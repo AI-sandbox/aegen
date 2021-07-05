9d04c46168b77c2a5d187a5d9fda04e0f91d8fad
3omni <noticiasmundiales99@gmail.com>
2021-07-05 01:46:32 -0700

    Add modules + fiex

--- src/models/VAEgen.py
+++ src/models/VAEgen.py
@@ -96,17 +96,40 @@ class Encoder(nn.Module):
                             wsize = self.window_size
                         else: wsize = self.window_size + (params['layer0']['size'] % self.window_size)
                     else: wsize = params[f'layer{i}']['size']
-                    modules[f'layer{i}_win{w}'] = FullyConnected(
-                        input      = wsize,
-                        output     = params[f'layer{i + 1}']['size'],
-                        dropout    = params[f'layer{i}']['dropout'],
-                        normalize  = params[f'layer{i}']['normalize'],
-                        activation = params[f'layer{i}']['activation'],
-                    )
+                    if self.latent_distribution == 'Gaussian':
+                        modules[f'layer{i}_win{w}_mu'] = FullyConnected(
+                            input      = wsize,
+                            output     = params[f'layer{i + 1}']['size'],
+                            dropout    = params[f'layer{i}']['dropout'],
+                            normalize  = params[f'layer{i}']['normalize'],
+                            activation = params[f'layer{i}']['activation'],
+                        )
+                        modules[f'layer{i}_win{w}_logvar'] = FullyConnected(
+                            input      = wsize,
+                            output     = params[f'layer{i + 1}']['size'],
+                            dropout    = params[f'layer{i}']['dropout'],
+                            normalize  = params[f'layer{i}']['normalize'],
+                            activation = params[f'layer{i}']['activation'],
+                        )
+                    else: 
+                        modules[f'layer{i}_win{w}'] = FullyConnected(
+                            input      = wsize,
+                            output     = params[f'layer{i + 1}']['size'],
+                            dropout    = params[f'layer{i}']['dropout'],
+                            normalize  = params[f'layer{i}']['normalize'],
+                            activation = params[f'layer{i}']['activation'],
+                        )
             ## We create funnels for each window.
-            self.funnel = []
-            for w in range(self.n_windows):
-                self.funnel.append(nn.Sequential(*[modules[f'layer{i}_win{w}'] for i in range(depth)]))
+            if self.latent_distribution == 'Gaussian':
+                self.funnel_mu, self.funnel_logvar = nn.ModuleList(), nn.ModuleList()
+                for w in range(self.n_windows):
+                    self.funnel_mu.append(nn.Sequential(*[modules[f'layer{i}_win{w}_mu'] for i in range(depth)]))
+                    self.funnel_logvar.append(nn.Sequential(*[modules[f'layer{i}_win{w}_logvar'] for i in range(depth)]))
+            else:
+                self.funnel = nn.ModuleList()
+                for w in range(self.n_windows):
+                    self.funnel.append(nn.Sequential(*[modules[f'layer{i}_win{w}'] for i in range(depth)]))
+
         elif self.shape == 'hybrid':
             raise Exception('Not implemented.')
         else: raise Exception('Unknown shape.')
@@ -123,27 +146,61 @@ class Encoder(nn.Module):
                 return o_mu, o_var
             else: return o
         elif self.shape == 'window-based': ## TODO: Implement conditioned window-based
-            os = [] ## Stores the outputs of each window.
-            for w in range(self.n_windows):
-                if w == self.n_windows - 1:
-                    x_windowed = x[..., w * self.window_size:]
-                else:
-                    x_windowed = x[..., w * self.window_size:(w + 1) * self.window_size]
-                ## Process each window with the corresponding funnel.
-                os.append(self.funnel[w](x_windowed))
-            ## Concat all outputs into a single o vector.
-            o = torch.cat(os, axis=-1)
+            if self.latent_distribution == 'Gaussian':
+                os_mu, os_logvar = [], [] ## Stores the outputs of each window.
+                for w in range(self.n_windows):
+                    if w == self.n_windows - 1:
+                        x_windowed = x[..., w * self.window_size:]
+                    else:
+                        x_windowed = x[..., w * self.window_size:(w + 1) * self.window_size]
+                    ## Process each window with the corresponding funnel.
+                    os_mu.append(self.funnel_mu[w](x_windowed))
+                    os_logvar.append(self.funnel_logvar[w](x_windowed))
+                ## Return means and logvars per window
+                return os_mu, os_logvar
+            else:
+                os = [] ## Stores the outputs of each window.
+                for w in range(self.n_windows):
+                    if w == self.n_windows - 1:
+                        x_windowed = x[..., w * self.window_size:]
+                    else:
+                        x_windowed = x[..., w * self.window_size:(w + 1) * self.window_size]
+                    ## Process each window with the corresponding funnel.
+                    os.append(self.funnel[w](x_windowed))
+                ## Concat all outputs into a single o vector.
+                o = torch.cat(os, axis=-1)
             return o
         elif self.shape == 'hybrid':
             raise Exception('Not implemented.')
         else: raise Exception('Unknown shape.')
         
 class Quantizer(nn.Module):
-    def __init__(self, latent_distribution, codebook = None):
+    def __init__(self, latent_distribution, params, shape='global', codebook = None, window_size=None):
         super().__init__()
         self.latent_distribution = latent_distribution
         self.codebook = codebook
+        self.shape = shape
+        ## Define the depth of the network.
+        depth = len(params.keys()) - 1
+        ## If shape is not global, define window size and number of windows.
+        self.window_size = window_size
+        if self.window_size is not None and self.shape != 'global':
+            self.n_windows = int(np.floor(params[f'layer{depth}']['size'] / self.window_size))
+        self.split_size = params['layer0']['size']
         
+    def _return_code(self, ze):
+        if ze.size(-1) != self.codebook.size(-1):
+            print(ze.shape, self.codebook.shape)
+            raise RuntimeError(
+                f'[Error] Invalid argument: ze.size(-1) ({ze.size(-1)}) must be equal to self.codebook.size(-1) ({self.codebook.size(-1)})'
+            )
+        sq_norm = (torch.sum(ze**2, dim = -1, keepdim = True) 
+                + torch.sum(self.codebook**2, dim = 1)
+                - 2 * torch.matmul(ze, self.codebook.t()))
+        _, argmin = sq_norm.min(-1)
+        zq = self.codebook.index_select(0, argmin.view(-1)).view(ze.shape)
+        return zq
+         
     def forward(self, ze):
         ## In Multi-Bernoulli LS, the quantizer
         ## uses a threshold to binarize the 
@@ -155,21 +212,13 @@ class Quantizer(nn.Module):
         ## computes the distances to the
         ## codebook vectors and takes the argmin.
         elif self.latent_distribution == 'Uniform':
-            if ze.size(-1) != self.codebook.size(-1):
-                raise RuntimeError(
-                    f'[Error] Invalid argument: ze.size(-1) ({ze.size(-1)}) must be equal to self.codebook.size(-1) ({self.codebook.size(-1)})'
-                )
-            # print(f'Codebook shape: {self.codebook.shape}')
-            # print(ze)
-            sq_norm = (torch.sum(ze**2, dim = -1, keepdim = True) 
-                    + torch.sum(self.codebook**2, dim = 1)
-                    - 2 * torch.matmul(ze, self.codebook.t()))
-            # print(f'Sq Norm: {sq_norm}')
-            _, argmin = sq_norm.min(-1)
-            # print(f'Codebook argmin: {argmin}')
-            # print(f'Codebook indices: {argmin.view(-1)}')
-            zq = self.codebook.index_select(0, argmin.view(-1)).view(ze.shape)
-
+            if self.shape == 'window-based':
+                zq = []
+                for w in range(self.n_windows):
+                    ze_windowed = ze[..., w * self.split_size: (w + 1) * self.split_size]
+                    zq.append(self._return_code(ze_windowed))
+                zq = torch.cat(zq, axis=-1)
+            else: zq = self._return_code(ze_windowed)
         return zq
     
     def backward(self, grad_zq):
@@ -177,7 +226,6 @@ class Quantizer(nn.Module):
         grad_ze = grad_zq.clone()
         return grad_ze, None
         
-
 class Decoder(nn.Module):
     def __init__(self, params, num_classes=None, shape='global', window_size=None):
         super().__init__()
@@ -187,8 +235,8 @@ class Decoder(nn.Module):
         depth = len(params.keys()) - 1
         ## If shape is not global, define window size and number of windows.
         self.window_size = window_size
-        if window_size is not None and shape != 'global':
-            self.n_windows = int(np.floor(params[f'layer{depth}']['size'] / window_size))
+        if self.window_size is not None and self.shape != 'global':
+            self.n_windows = int(np.floor(params[f'layer{depth}']['size'] / self.window_size))
         
         ## Shape global modules definitions.
         if self.shape == 'global':
@@ -225,7 +273,7 @@ class Decoder(nn.Module):
                         activation = params[f'layer{i}']['activation'],
                     )
             ## We create funnels for each window.
-            self.funnel = []
+            self.funnel = nn.ModuleList()
             for w in range(self.n_windows):
                 self.funnel.append(nn.Sequential(*[modules[f'layer{i}_win{w}'] for i in range(depth)]))
             self.split_size = params['layer0']['size']
@@ -291,20 +339,33 @@ class AEgen(nn.Module):
                 torch.empty(
                     params['codebook_size'], params['decoder']['layer0']['size']
                 ).uniform_(0, 1)
-            )
+            ), requires_grad = True
         ) if self.latent_distribution == 'Uniform' else None
         self.quantizer = Quantizer(
             latent_distribution = self.latent_distribution,
-            codebook = self.codebook
+            codebook = self.codebook, 
+            shape = self.shape, 
+            params = params['decoder'],
+            window_size=self.window_size
         )
     
     ## Variational Auto-encoder: Gaussian latent space
     def reparametrize(self, mu, logvar):
-        if self.training or sample_mode:
-            std = torch.exp(0.5 * logvar)
-            eps = torch.randn_like(std)
-            return mu + std * eps
-        else: return mu
+        if self.shape == 'window-based':
+            if self.training or sample_mode:
+                z = []
+                for i in range(len(mu)):
+                    std = torch.exp(0.5 * logvar[i])
+                    eps = torch.randn_like(std)
+                    z.append(mu[i] + std * eps)
+                return torch.cat(z, axis=-1)
+            else: return torch.cat(mu, axis=-1)
+        else:
+            if self.training or sample_mode:
+                std = torch.exp(0.5 * logvar)
+                eps = torch.randn_like(std)
+                return mu + std * eps
+            else: return mu
         
     # def sample(self, )
 
