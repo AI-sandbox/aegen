c07d374a4b2178760db3a51bbcba96adb22fc8e4
3omni <noticiasmundiales99@gmail.com>
2021-08-16 02:39:00 -0700

    instant script nero

--- scripts/instant.sh
+++ scripts/instant.sh
@@ -1,28 +1,32 @@
-# !/bin/bash
-# Sample command: ./instant.sh -e 1 -s "[C] Imputation: 10K 512-64 ReLU" --verbose
-while [[ $# -gt 0 ]]; do 
-    case $1 in
-        -v | --verbose ) shift; verbose="True" ;;
-        -e | --exp ) shift; exp=$1 ;;
-        -s | --summary ) shift; summary=$1 ;;
-        * ) echo "Incorrect argument"; exit 1 ;;
-    esac; shift
+#!/bin/bash
+source ini.sh
+for ARGUMENT in "$@" 
+do
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
+    case "$KEY" in
+            cluster)     cluster=${VALUE} ;;
+            experiment) experiment=${VALUE} ;;     
+            *)   
+    esac    
 done
 
-if [[ -z $verbose ]]; then verbose="False"; fi
-if [[ -z $exp ]]; then echo "Missing experiment number."; exit 1; fi
-if [[ -z $summary ]]; then echo "Missing experiment summary."; exit 1; fi
+if [[ -z $cluster ]]; then cluster=$CLUSTER; fi
+if [[ -z $experiment ]]; then echo "Missing experiment number."; exit 1; fi
 
-echo "Executing instant experiment ($summary)(#$exp)"
-mkdir -p $OUT_PATH/experiments/exp$exp
-cp $USER_PATH/params.yaml $OUT_PATH/experiments/exp$exp/
-if srun -u --gres=gpu:2,gpumem:11G -p gpi.compute -c 8 --time 01:59:59 --mem 32GB python3 $USER_PATH/src/trainer.py \
-    --params $OUT_PATH/experiments/exp$exp/params.yaml \
-    --experiment "$summary" \
-    --verbose $verbose \
-    --num $exp \
-    --evolution False \
-    --conditional False \
-    --imputation True; then
-    echo "Success!"
-else echo 'Fail!'; fi
+echo "[$CLUSTER] Executing experiment #$experiment"
+rm -rf $OUT_PATH/experiments/exp$experiment
+mkdir -p $OUT_PATH/experiments/exp$experiment
+cp $USER_PATH/params.yaml $OUT_PATH/experiments/exp$experiment/
+touch $OUT_PATH/experiments/exp$experiment/exp$experiment.log
+chmod +rwx $OUT_PATH/experiments/exp$experiment/exp$experiment.log
+
+if python3 $USER_PATH/src/trainer.py \
+--species human \
+--chr 22 \
+--params $OUT_PATH/experiments/exp$experiment/params.yaml \
+--num $experiment \
+--verbose True \
+--evolution False;
+then echo "[$CLUSTER] Success!"
+else echo "[$CLUSTER] Fail!"; fi
