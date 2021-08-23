b349628857e3a8f9549c04199a8298a26f125b9a
3omni <noticiasmundiales99@gmail.com>
2021-08-23 14:45:04 +0000

    fix compression for discrete aes

--- src/eval/compressors.py
+++ src/eval/compressors.py
@@ -6,9 +6,27 @@ import numpy as np
 import blosc2 as blosc
 from abc import ABC, abstractmethod
 
-sys.path.insert(0, '/home/users/geleta/aegen/src')
+sys.path.insert(0, '/home/geleta/aegen/src')
 from models.metrics import *
-from eval.utils import *
+
+def load_model(experiment):
+    with open(f'/local-scratch/mrivas/experiments/exp{experiment}/params.yaml', 'r') as f: 
+        params = yaml.safe_load(f)
+    model_params = params['model']
+    
+    ae = aegen(model_params, conditional=False, sample_mode=False)
+    
+    state = torch.load(f'/local-scratch/mrivas/experiments/exp{experiment}/aegen_weights_{experiment}.pt')
+    stats = torch.load(f'/local-scratch/mrivas/experiments/exp{experiment}/aegen_stats_{experiment}.pt')
+    print(f"Model has been trained during {stats['epoch']} epochs.")
+    print(f"Retrieving the  best checkpoint, from epoch {stats['best_epoch']}.")
+    ae.load_state_dict(state['weights'], strict=False)
+    ae.eval();
+    print("Model ready for testing.")
+    device  = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using {torch.cuda.device_count()} GPU(s).")
+    print(f'Sending model to device {torch.cuda.get_device_name()}.')
+    return ae.to(device), state
 
 ## Abstract class of a compressor.
 ## [experiment] defines the ID of the trained ae to load.
@@ -21,12 +39,13 @@ class Compressor(ABC):
     ## of the compressed data).
     ## [latent_type] is the data type of the latent factors.
     def __init__(self, experiment):
-        self.ae, _ = load_model(experiment)
-        self.distribution = _['distribution']
-        self.isize = _['isize']
-        if _['shape'] == 'window-based': 
-            self.bsize = int(_['isize'] / _['window_size'] * _['bsize'])
-        else: self.bsize = int(_['bsize'])
+        self.ae, self.stats = load_model(experiment)
+        self.distribution = self.stats['distribution']
+        self.isize = self.stats['isize']
+        self.wsize = self.stats['window_size']
+        if self.stats['shape'] == 'window-based': 
+            self.bsize = int(self.stats['isize'] / self.wsize * self.stats['bsize'])
+        else: self.bsize = int(self.stats['bsize'])
         
         if self.distribution == 'Uniform':
             self.latent_type = np.dtype('B')
@@ -65,7 +84,9 @@ class LZAE(Compressor):
     ## Depending on the capabilities of the GPU, the batch size can be increased.
     def compress(self, data, lz_algorithm='zstd', shuffle=blosc.BITSHUFFLE, batch_size=512, save_z=None, save_r=None):
         
-        latent = np.empty((0, self.bsize), self.latent_type)
+        if self.distribution == 'Uniform':
+            latent = np.empty((0, int(self.isize / self.wsize)), self.latent_type)
+        else: latent = np.empty((0, self.bsize), self.latent_type)
         residual = np.empty((0, self.isize), bool)
 
         print(f'Autoencoder processing data of shape {data.shape}...')
@@ -77,13 +98,18 @@ class LZAE(Compressor):
                 x = torch.from_numpy(data[i:i + batch_size,:]).float().cuda()
             ## Obtain latent representation
             z = self.ae.encoder(x, None)
-            if self.distribution == 'Uniform': _, z, _, _, _ = self.ae.quantizer(z)
+            if self.distribution == 'Uniform': indices, z, _, _, _ = self.ae.quantizer(z)
             elif self.distribution == 'Multi-Bernoulli': z = self.ae.quantizer(z)
             elif self.distribution == 'Gaussian': z = torch.cat(z[0], axis=-1)
             ## Obtain reconstruction
             o = (self.ae.decoder(z, None) > 0.5).float().cpu().detach().numpy()
             ## Store latent representation
-            z = z.cpu().detach().squeeze(0).numpy().astype(self.latent_type)
+            z = z.cpu().detach().squeeze(0).numpy()
+            if self.distribution == 'Multi-Bernoulli':
+                z = 1/2*z + 1/2
+            elif self.distribution == 'Uniform': 
+                z = indices.float().cpu().detach().numpy()
+            z = z.astype(self.latent_type)
             latent = np.vstack((latent, z))
             ## Compute residual
             x = x.cpu().detach().numpy()
@@ -97,15 +123,19 @@ class LZAE(Compressor):
         
         xbin, zbin, rbin = data.tostring(), latent.tostring(), residual.tostring()
         print(f'Compressing data of size {len(xbin)} bytes...')
+        xcom = blosc.compress(xbin, typesize=1, cname=lz_algorithm, shuffle=shuffle)
+        print(f'Compressed size of raw data: {len(xcom)} bytes.')
         zcom = blosc.compress(zbin, typesize=elem_tsize(latent), cname=lz_algorithm, shuffle=shuffle)
         rcom = blosc.compress(rbin, typesize=elem_tsize(residual), cname=lz_algorithm, shuffle=shuffle)
         end = time.time()
-        print(f'Compressed size: {len(zcom) + len(rcom)} bytes.')
+        print(f'Compressed size of ae-processed data: {len(zcom) + len(rcom)} bytes.')
         print('='*50)
         factor = len(xbin) / (len(zcom) + len(rcom))
+        cfactor = len(xcom) / (len(zcom) + len(rcom))
         print(f'Average reconstruction error: {np.sum(residual.astype(float))/data.shape[1]}.')
         print(f'Compression ratio: {np.round(1/factor, 2)}.')
         print(f'Compression factor: x{np.round(factor, 2)}.')
+        print(f'CC factor: x{np.round(cfactor, 2)}.')
         print(f'Compression time: {np.round(end - ini, 2)} seconds.')
         print('='*50)
         del data, latent, residual
@@ -134,8 +164,15 @@ class LZAE(Compressor):
         zbin = blosc.decompress(zcom)
         rbin = blosc.decompress(rcom)
         
-        z = np.frombuffer(zbin, dtype=self.latent_type).reshape(-1,self.bsize)
-        r = np.frombuffer(rbin, dtype=bool).reshape(-1,self.isize)
+        if self.distribution == 'Uniform':
+            indices = torch.from_numpy(np.frombuffer(zbin, dtype=self.latent_type).reshape(-1,int(self.isize/self.wsize))).cuda().long()
+            z = self.ae.quantizer.codebook.weight.index_select(0, indices.view(-1)).view((-1,self.bsize)).cpu().detach().numpy()
+            del indices
+        else:
+            z = np.frombuffer(zbin, dtype=self.latent_type).reshape(-1,self.bsize).astype(float)
+        r = np.frombuffer(rbin, dtype=bool).reshape(-1,self.isize).astype(int)
+        if self.distribution == 'Multi-Bernoulli':
+            z = 2*z - 1
 
         reconstructed = np.empty((0, self.isize), bool)
         print(f'Autoencoder processing latent data of shape {z.shape}...')
